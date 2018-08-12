af55df9ba887a49b78d9aaaca6090650f7473e88
luaemu <LuaEmuInquiries@gmail.com>
2018-08-12 17:18:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function main() {
 	$("body").hide();
-	$("body").fadeIn(800);
+	$("body").fadeIn(900);
+	$('.slideButton').next().hide();
+	$('.slideButton').on('click', function(event) {
+		$('.slideButton').next().slideToggle(800);
+		$('.slideButton').css('background-color', '#566573');
+		$('.slideButton').css('color', 'white');
+	});
 }
 	$(document).ready(main);
