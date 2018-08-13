adbe307891aa7f92c3d004aa68bdc13e6a6aeabe
luaemu <LuaEmuInquiries@gmail.com>
2018-08-12 20:36:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-<<<<<<< HEAD
 function main() {
 	$("body").hide();
 	$("body").fadeIn(900);
-	$('.slideButton').next().hide();
-	$('.slideButton').on('click', function(event) {
-		$('.slideButton').next().slideToggle(800);
-		$('.slideButton').css('background-color', '#566573');
-		$('.slideButton').css('color', 'white');
+	$('.aboutButton').next().hide();
+	$('.aboutButton').on('click', function(event) {
+		$('.aboutButton').next().slideToggle(800);
+		$('.aboutButton').css('background-color', '#566573');
+		$('.aboutButton').css('color', 'white');
 	});
 }
-=======
 function main() {
 	$("body").hide();
 	$("body").fadeIn(900);
-	$('.slideButton').next().hide();
-	$('.slideButton').on('click', function(event) {
-		$('.slideButton').next().slideToggle(800);
-		$('.slideButton').css('background-color', '#566573');
-		$('.slideButton').css('color', 'white');
+	$('.aboutButton').next().hide();
+	$('.aboutButton').on('click', function(event) {
+		$('.aboutButton').next().slideToggle(800);
+		$('.aboutButton').css('background-color', '#566573');
+		$('.aboutButton').css('color', 'white');
 	});
 }
->>>>>>> 89d6dfa5fcfa092b4f35a9c6c88aedc2c6c6fc82
 	$(document).ready(main);
